3b7f53d5b3f82d62cc39b4a04e328a448edd78a7
vadasambar <surajrbanakar@gmail.com>
2023-09-06 11:02:12 +0530

    fix: code had logical error

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Trying out `golang.org/x/mod/semver` package's `Compare` function.
+
+### Output
+![](2023-09-06-11-02-06.png)
--- main.go
+++ main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"golang.org/x/mod/semver"
 )
 
+var version = "v0.4.0"
+
 func main() {
-	v1 := "v0.3.0"
-	v2 := "v0.4.0"
-	c := semver.Compare(v2, v1)
-	if c > 1 || c == 1 {
-		fmt.Println("v2 is greater than v1")
+	if len(os.Args) != 2 {
+		log.Fatal("specify exactly one argument/version")
+	}
+
+	input := os.Args[1]
+
+	c := semver.Compare(input, version)
+	if c > 0 {
+		fmt.Printf("%s is a higher version than %s\n", input, version)
+	} else if c == 0 {
+		fmt.Printf("%s is the same version as %s\n", input, version)
 	} else {
-		fmt.Println("v1 is greater than v2")
+		fmt.Printf("%s is a lower version than %s\n", input, version)
 	}
 
 }
